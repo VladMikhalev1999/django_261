7b04ca8f1ffceaba3a5b9a954fbb71b4c7e4122e
Владислав Михалев <mikhalev@it-ktk.local>
2024-03-30 10:35:48 +0500

    Пара 3: CRUD

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import index, about
+from .views import *
 
 
 app_name = 'app'
 
 urlpatterns = [
-    path('index/', index, name='index'),
-    path('about/', about, name='about')
+    path('category/', index, name='index'),
+    path('about/', about, name='about'),
+    path('category/add/', add_category, name='category_add'),
+    path('category/<int:id>/', add_category, name='category_edit')
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Category
 
@@ -14,13 +14,50 @@ def index(request):
        {'id': 3, 'name': 'cat 3'}
    ]'''
     categories = Category.objects.all().order_by("name")
-    try:
-        category = Category.objects.get(pk=100)
-    except:
-        pass
-    category = Category.objects.filter(pk=100)
+    if request.method == "POST":
+        id = request.POST.get("id")
+        method = request.POST.get("method")
+        if method == "DELETE":
+            try:
+                category = Category.objects.get(pk=id)
+                category.delete()
+            except:
+                return redirect("app:index")
     return render(request, "app/index.html", {"cate": categories})
 
 
 def about(request):
     return render(request, "app/about.html")
+
+
+def add_category(request, id=None):
+    errors = {}
+    c = None
+    name = ""
+    if id is not None:
+        try:
+            c = Category.objects.get(pk=id)
+            name = c.name
+        except:
+            return redirect("app:category_add")
+    if request.method == "POST":
+        method = request.POST.get("method", "POST")
+        name = request.POST.get("name")
+        if name == "":
+            errors["name"] = "Поле наименование не может быть пустым."
+        if errors:
+            return render(request, "app/category_add.html", {"id": id, "name": name, "errors": errors})
+        try:
+            if method == "POST":
+                c = Category()
+                c.name = name
+                c.save()
+            elif method == "PUT":
+                c = Category.objects.get(pk=id)
+                c.name = name
+                c.save()
+            return redirect("app:index")
+        except:
+            errors["name"] = "Такая категория уже существует."
+            return render(request, "app/category_add.html", {"errors": errors, "id": id, "name": name})
+    return render(request, "app/category_add.html", {"id": id, "name": name})
